59b127bd56d79ea806664bbf926f0f7e6d75ad1b
Sabrina Estévez <sesmo.dev@gmail.com>
2024-01-13 12:45:16 +0000

    añadido boton borrar

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -2,37 +2,37 @@ import React, { useState } from "react";
 
 const Input = () => {
 
-    const [inputValue, setInputValue] = useState('');
+    const [inputValue, setInputValue] = useState("");
     const [list, setList] = useState([]);
 
-    const validate = (value) => {
-        setInputValue(value);
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
     }
 
     const addItem = () => {
-        list.unshift(inputValue);
-        setList(list);
-        setInputValue('');
+        if (inputValue.trim() !== ""){
+            setList([inputValue, ...list]);
+        setInputValue("");
+        }      
     }
     
-    const _handleKeyDown = () => {
-        function(e) {
-            if (e.key === "Enter"){
-                addItem();
+    const handleKeyDown = (e) => {
+         if (e.key === "Enter"){
+            e.preventDefault();    
+            addItem();
             }
         }
-    }
+
+    const onDelete = (index) => {
+        const updatedItems = [...list.slice(0, index), ...list.slice(index + 1)];
+        setList(updatedItems);
+      }
 
     return (
         <div className="text-center">
             <h1 className="text-center mt-5">¿Empezamos a organizarnos?</h1>
             <br></br>
-            <p>Añadir nueva tarea:</p>
-            <input type="text" onChange={e => validate(e.target.value)} onKeyDown={this._handleKeyDown} value={inputValue} />
-
-            {/* <button onClick={() => addItem()}>
-                Añadir
-            </button> */}
+            <input type="text" onChange={handleInputChange} onKeyDown={handleKeyDown} value={inputValue} placeholder="Añadir nueva tarea"/>
 
             <ul>
                 <h5 className="mt-5 text-center">Lista de tareas pendientes:</h5>
@@ -40,6 +40,7 @@ const Input = () => {
                     list.map((el, i) => (
                         <li key={i}>
                             {el}
+                            <button onClick={() => onDelete(i)}>Cruz</button>
                         </li>
                     ))
                 }
