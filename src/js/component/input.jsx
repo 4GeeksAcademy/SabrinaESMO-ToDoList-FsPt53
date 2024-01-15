e62593a038909540f951c18d1d69622a17b93166
Sabrina Estévez <sesmo.dev@gmail.com>
2024-01-15 20:08:11 +0000

    progreso maquetación

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -10,40 +10,42 @@ const Input = () => {
     }
 
     const addItem = () => {
-        if (inputValue.trim() !== ""){
+        if (inputValue.trim() !== "") {
             setList([inputValue, ...list]);
-        setInputValue("");
-        }      
-    }
-    
-    const handleKeyDown = (e) => {
-         if (e.key === "Enter"){
-            e.preventDefault();    
-            addItem();
-            }
+            setInputValue("");
         }
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            addItem();
+        }
+    }
 
     const onDelete = (index) => {
         const updatedItems = [...list.slice(0, index), ...list.slice(index + 1)];
         setList(updatedItems);
-      }
+    }
 
     return (
         <div className="text-center">
             <h1 className="text-center mt-5">¿Empezamos a organizarnos?</h1>
             <br></br>
-            <input type="text" onChange={handleInputChange} onKeyDown={handleKeyDown} value={inputValue} placeholder="Añadir nueva tarea"/>
+            <input type="text" onChange={handleInputChange} onKeyDown={handleKeyDown} value={inputValue} placeholder="Añadir nueva tarea" />
 
-            <ul>
-                <h5 className="mt-5 text-center">Lista de tareas pendientes:</h5>
-                {
+            <ul className="listContainer mt-3 w-75 mx-auto p-2">
+                <h5 className="mt-1 text-center">Lista de tareas pendientes:</h5>
+                <div>{ 
                     list.map((el, i) => (
-                        <li key={i}>
+                        <li key={i} className="itemContainer text-start">
+                            <i class="fa-solid fa-thumbtack">&emsp;</i>
                             {el}
-                            <button onClick={() => onDelete(i)}>Cruz</button>
+                            <button onClick={() => onDelete(i)}><i class="trashCan fa-regular fa-trash-can"></i></button>
                         </li>
                     ))
                 }
+                    </div>
             </ul>
         </div>
     );
